9fa168f4ca83e40479daa4eba39741ca32db6f21
Tianyu Justin Yang <tyjyang@mit.edu>
2021-03-28 18:23:46 -0500

    fixing ntuple filename

--- lib/helper.py
+++ lib/helper.py
@@ -386,10 +386,10 @@ def get_ntuple_filename(delphes_file_path, particle_variable):
 	particles = particle_variable.keys()
 
 	for i, particle in enumerate(sorted(particles)):
-		ntuple_content += particle + "-"
 		if i == len(particles) - 1: # use "-" btwn particles
 			variables = string_to_list(particle_variable[particle])
-			ntuple_content += "_" + variable_separator.join(variables)
+			ntuple_content += ':' + variable_separator.join(variables)
+		else: ntuple_content += particle + "-"
 
 	return ntuple_content + '.root'
 
