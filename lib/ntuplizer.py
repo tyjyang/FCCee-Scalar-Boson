435d05c2fc64d752b93709144b33ba685f796360
Tianyu Justin Yang <tyjyang@mit.edu>
2021-04-08 21:01:34 -0500

    fixing select_ptcl_var_highest()

--- lib/ntuplizer.py
+++ lib/ntuplizer.py
@@ -262,7 +262,7 @@ def select_ptcl_var_highest(delphes_file, event, particles, var, var_in_delphes,
 						cand_max.clear()
 						cand_max[ptcl] = cand
 		else:
-			for i_cand, cand in enumerate(ptcl_cands[ptcl]):
+			for i_cand, cand in enumerate(ptcl_cands):
 				ptcl_cands = {ptcl:cands}
 				var_calc = calc_ptcl_var_by_idx(delphes_file, event,
 				                                ptcl_cands, var)
